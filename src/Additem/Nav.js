0c97cd4d3e428dcc6634e0f9bc34965acda8793c
Souravdhar7595 <148620289+Souravdhar7595@users.noreply.github.com>
2023-10-31 15:06:20 +0530

    first commit

--- src/Additem/Nav.js
+++ src/Additem/Nav.js
@@ -36,7 +36,6 @@ export default function Navbar(props) {
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
          
